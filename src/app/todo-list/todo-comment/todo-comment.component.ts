bb2a42cdecb595f5b6601092fc4b09b099270b53
ericboy0224 <ericboy0224@gmail.com>
2021-07-30 10:00:06 +0800

    optimized comment component

--- src/app/todo-list/todo-comment/todo-comment.component.ts
+++ src/app/todo-list/todo-comment/todo-comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, Output, EventEmitter,ElementRef, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, Renderer2, ViewChild, ElementRef } from '@angular/core';
 import { Todo } from '../todo.model';
 @Component({
     selector: 'app-todo-comment',
@@ -8,45 +8,32 @@ import { Todo } from '../todo.model';
 export class TodoCommentComponent implements OnInit {
     @Input() todo: Todo = { title: '', comment: '', completed: false, editing: false, date: '' };
     newTitle: string = "";
-    titleEditing= false;
+    titleEditing = false;
 
-    @Output() edited = new EventEmitter<{newTitle: string,newComment: string }>();
+    @Output() edited = new EventEmitter<{ newTitle: string, newComment: string }>();
 
-    @ViewChild('titlevalue',{static:false})
-    set titlevalue(element: ElementRef<HTMLInputElement>){
-        if(element){
-            element.nativeElement.focus();
-        }
+    constructor(public render2: Renderer2) {
     }
-    constructor() { }
 
 
     ngOnInit(): void {
     }
-    titleEdited(title:string){
-        this.todo.title = title;
-        this.titleEditing = false;
 
-    }
-    editTitle(){
-        this.titleEditing = true;
-    }
 
-    // editTodo(newComment: string): void{
-    //     const comment = newComment.trim();
-    //     this.todo.comment = comment.length >0 ? comment :'write some comments...';
-    //     this.todo.editing = false;
-    // }:
-    editTodo(titlevalue: HTMLInputElement,commentvalue: HTMLTextAreaElement): void {
+    change(): void {
+        this.render2.selectRootElement('#comment').focus();
+    }
+    editTodo(titlevalue: HTMLInputElement, commentvalue: HTMLTextAreaElement): void {
 
         this.edited.emit({
             newTitle: titlevalue.value,
-            newComment:commentvalue.value
+            newComment: commentvalue.value
         })
     }
 
-    closeEdit(todo: Todo): void{
-    todo.editing = false;
+    closeEdit(todo: Todo): void {
+        todo.editing = false;
+        todo.comment = todo.comment.trim().length > 0 ? todo.comment : 'Write some comments...';
     }
 
 }
